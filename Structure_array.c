3a9283423764864912da6d6841ed0a5b57ab34c7
raksha58 <59732018+raksha58@users.noreply.github.com>
2020-01-10 18:59:06 +0530

    Update Structure_array.c

--- Structure_array.c
+++ Structure_array.c
@@ -12,37 +12,44 @@ struct Student
 
 int main()
 {
-        struct Student arr_student;
-        int j;
-        float sum = 0;
         
-        printf("\nEnter details of student %d\n\n");
+        int number,i,j;
+        printf("Enter no of students:");
+        scaf("%d", &number);
+    
+        struct Student arr_student[number];
+        
+        
+        printf("\nEnter details of student %d\n\n",number+1);
+    
+       for(i=0;i<number;i++)
+       {
+          printf("Enter name: ");
+          scanf("%s", arr_student[i].name);
 
-        printf("Enter name: ");
-        scanf("%s", arr_student.name);
-
-        printf("Enter roll no: ");
-        scanf("%d", &arr_student.roll_no);
-
-        for(j = 0; j < SUBJECTS; j++)
-        {
-            printf("Enter marks: ");
-            scanf("%f", &arr_student.marks[j]);
-        }
+          printf("Enter roll no: ");
+          scanf("%d", &arr_student[i].roll_no);
+ 
+          printf("Enter marks: ");
+          for(j = 0; j < SUBJECTS; j++)
+           {
+              scanf("%f", &arr_student[i].marks[j]);
+           }
+        }     
     printf("\n");
 
-    printf("Name\tRoll no\tAverage\n\n");
+    printf("\nDetails are as follow:\n");
+      for(i=0;i<number;i++)
+      {  
+          float sum = 0;
 
-     sum = 0;
-
-        for(j = 0; j < SUBJECTS; j++)
-        {
-            sum += arr_student.marks[j];
-        }
-        printf("%s\t%d\t%.2f\n",
-             arr_student.name, arr_student.roll_no, sum/SUBJECTS);
-
-
-
+          for(j = 0; j < SUBJECTS; j++)
+           {
+              sum += arr_student[i].marks[j];
+           }
+           printf("Name:%s\tRoll no:%d\tAverage:%.2f\n",
+             arr_student[i].name, arr_student[i].roll_no, sum/SUBJECTS);
+      }
+    
     return 0;
 }
